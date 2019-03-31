6e33095fb913a7e086ee4c726478b8f3e9188451
Jeffrey <jeffreyweiok@163.com>
2019-04-01 00:32:28 +0800

    json

--- JFSwiftProject/JFController.swift
+++ JFSwiftProject/JFController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Moya
 import RxSwift
+import SwiftyJSON
 
 class JFController: UIViewController {
     let vm = JFHomeViewModel.init()
@@ -16,15 +17,42 @@ class JFController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let res  = JFApiResponse()
+        let res = JFApiResponse()
         self.navigationItem.title = "Jeffrey"
         let provider = MoyaProvider<JFApi>()
         let ob = provider.rx.request(.home)
 //        let ob2 = self.vm.obHome
-        self.vm.obHome.subscribe(onNext: { any in
-//            let dic = any as! Dictionary<String, String>
+        self.vm.obHome.subscribe(onNext: { value in
+            let  s = value as! Dictionary<String,Any>
+            let x = s["reason"] as! String
+            print("")
+//            let test = value as AnyObject
+//            let x = test.raw
+//            if let jsonData = value.data(using: String.Encoding.utf8, allowLossyConversion: false) {
+
+
+//                do {
+//                    let dic = ["key":["subKey":"subValue"]]
+//                    let json = try JSON(data: jsonData).dictionary
+//                    let reason  = json["reason"].string!
+//                    print("")
+//                } catch {
+//                }
+
+//            }
+
+//            let dic = any as! Dictionary
 //            let code = dic["reason"]
-            print(any)
+            print("")
+
+//            do {
+//                let json = any as! Dictionary<String, String>
+//                let a = json["reason"] as String
+//                print("")
+//            } catch {
+//            }
+
+
         }, onError: { error in
             print(error)
         })
