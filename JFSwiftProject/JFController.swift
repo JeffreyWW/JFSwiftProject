b4340a7d4cdc259ff4aba844ca8974bc610cb346
Jeffrey <jeffreyweiok@163.com>
2019-04-07 18:39:42 +0800

    输入正确

--- JFSwiftProject/JFController.swift
+++ JFSwiftProject/JFController.swift
@@ -19,52 +19,40 @@ import MBProgressHUD
 
 
 class JFController: UIViewController {
+
     let disposeBag = DisposeBag()
-    let vm = JFHomeViewModel.init()
+    lazy var vm: JFHomeViewModel = {
+        let phone = self.txtPhone.rx.text.orEmpty.asDriver()
+        let password = self.txtPassword.rx.text.orEmpty.asDriver()
+        return JFHomeViewModel(input: (phone: phone, password: password))
+    }()
     @IBOutlet weak var txtPhone: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var lbTest: UILabel!
-    @IBOutlet weak var btnTest: UIButton!
-    let obTest = Observable<String>.create { observer in
-        print("2")
-        observer.onNext("1111")
-        observer.onError(JFApiError.tooManyTime)
-//        observer.onCompleted()
-        return Disposables.create()
-    }
-
+    @IBOutlet weak var btnNext: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let dr: Driver = Driver.combineLatest(self.txtPhone.rx.text.orEmpty.asDriver(), self.txtPassword.rx.text.orEmpty.asDriver())
-//        self.btnTest.rx.tap.asDriver().withLatestFrom(dr).flatMap { () -> SharedSequence<Sharing, R> in
-//        }
-//        Driver<Bool>.just(true).flatMap { e -> SharedSequence<Sharing, R> in
-//        }
+        self.binding()
 
-
-        //输入过滤的bool给到vm的check
-//        self.txtTest.rx.text.map { s -> Bool in
-//            return s!.count > 11
-//        }.bind(to: self.vm.check)
-        //vm的check值绑定到btn上,btn则会在输入11位后才enable
-//        self.vm.check.bind(to: self.btnTest.rx.isEnabled)
 
     }
 
+    private func binding() {
+        self.vm.btnEnable.drive(self.btnNext.rx.isEnabled)
+    }
+
     @IBAction func clickToast() {
-        let s2 = ("Jeff", "tom")
-        s2.0
-        Driver.just("1").flatMap { e -> Driver<String> in
-            Driver.just("2")
-        }.drive(onNext: { i in
-            print(i)
-        })
+
+
+        //提示确认还是取消
+//        let a = Observable.create { (observer: AnyObserver<Element>) in  }.asDriver(onErrorJustReturn: false)
 
         //把错误转换为驱动的某值
-        //input包含了所有的输入和选择(selected)等
+        //input包含了所有的输入和选择(selected)等,并包含了一个确认驱动,此驱动依赖点击了确定还是取消,分别为true和false,
         //输出设计:根据点击的时候和验证结果,为一个间接驱动
-        //在间接基础上,会有一个提示的驱动,提示驱动作为输出,驱动控制器弹提示
+        //在间接基础上,会有一个错误提示的驱动,提示驱动作为输出,驱动控制器弹提示
+        //简洁基础上,会有一个提示确定继续的驱动,驱动外部点击取消还是确认,确认just"true"
         //在间接基础上,会有一个网络请求驱动,也是间接驱动
         //网络请求基础上,分为失败驱动和数据获取的驱动,以驱动当前控制器继续下一步操作
         //失败驱动驱动控制器扩展的分类某属性
--- JFSwiftProject/Jeffrey/ViewModels/JFHomeViewModel.swift
+++ JFSwiftProject/Jeffrey/ViewModels/JFHomeViewModel.swift
@@ -11,12 +11,41 @@ import SwiftyJSON
 import RxCocoa
 
 class JFHomeViewModel: ReactiveCompatible {
+    //输入必须确定,输出其实也必须会确定,但那样的话,必须在初始化就写出如何输出,内部的每个输出无法用单独的变量,因为
+    //索性用lazy
+    let input: (phone: Driver<String>, password: Driver<String>)
 
-    init() {
-//        self.phone.map { s -> Bool in
-//            return true
-//        }.bind(to: self.check)
+    init(input: (phone: Driver<String>, password: Driver<String>)) {
+        self.input = input
     }
+
+    lazy var output: (btnEnable: Driver<Bool>, done: Driver<Bool>) = {
+        return (btnEnable: self.btnEnable, done: Driver.just(true))
+    }()
+
+
+    //变量之前无法引用,要引用则只能是懒加载或者计算
+    var btnEnable: Driver<Bool> {
+        let phoneHasInput = self.input.phone.map { s -> Bool in
+            s.count > 0
+        }
+        let passwordHasInput = self.input.password.map { s -> Bool in
+            s.count > 0
+        }
+        return Driver.combineLatest(phoneHasInput, passwordHasInput).map { phone, password -> Bool in
+            return phone && password
+        }
+    }
+
+
+//    lazy var output: (btnEnable: Driver<Bool>, done: Driver<Bool>) = {
+//        return (btnEnable: self.btnEnable, done: Driver.just(false))
+//    }()
+
+//    private lazy var btnEnable = {
+//        return Driver.just(true)
+//    }()
+
 
     var checkPhone: BehaviorRelay<Bool?> = BehaviorRelay<Bool?>(value: true)
     var checkPwd: BehaviorRelay<Bool?> = BehaviorRelay<Bool?>(value: true)
